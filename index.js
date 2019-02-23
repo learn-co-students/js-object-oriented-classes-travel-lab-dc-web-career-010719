b729fd91d6dd97c10e9995c961a185ab0afc117c
Serven Maraghi <maraghiserven@gmail.com>
2019-02-23 18:47:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 
+class Driver {
+    constructor(name, startDate) {
+        this.name = name
+        this.startDate = new Date(startDate)
+    }
+}
+
+Driver.prototype.yearsExperienceFromBeginningOf = function(year) {
+   return year - this.startDate.getFullYear()
+}
+
+class Route {
+    constructor(beginningLocation, endingLocation) {
+        this.beginningLocation = beginningLocation
+        this.endingLocation = endingLocation
+    }
+}
+
+Route.prototype.blocksTravelled = function() {
+    let verticalBlocks = this.endingLocation.vertical - this.beginningLocation.vertical
+    let horizontalBlocks =  eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal)
+    return Math.abs((verticalBlocks) + (horizontalBlocks))
+}
+   
+Route.prototype.estimatedTime = function(peak) {
+    if(peak) {
+        return this.blocksTravelled() / 2
+    } else {
+        return this.blocksTravelled() / 3
+    }
+}
+
+   
+   
+   
+
+   
+
+
+
+let eastWest = [
+    '1st Avenue',
+    '2nd Avenue',
+    '3rd Avenue',
+    'Lexington Avenue',
+    'Park',
+    'Madison Avenue',
+    '5th Avenue'
+  ];
