9201484a232c51d5ab86d0d3c9d3ea2d13e1642d
shannon-nabors <shannon.nabors@gmail.com>
2019-02-23 19:43:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 
+class Driver {
+  constructor(name, date) {
+    this.name = name
+    this.startDate = new Date(date)
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    return (year - this.startDate.getFullYear())
+  }
+}
+
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+]
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled() {
+    let vDist = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+    let hDist = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal))
+    return vDist + hDist
+  }
+
+  estimatedTime(peakHours) {
+    if (peakHours) {
+      return this.blocksTravelled() / 2
+    } else {
+      return this.blocksTravelled() / 3
+    }
+  }
+}
